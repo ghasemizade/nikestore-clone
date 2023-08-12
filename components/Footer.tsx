6a7b6ef9d85dbec96bf32aec4fc86b1565a14475
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-12 12:10:18 +0330

    feat (footer): add footer component in component folder

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+import clsx from "clsx";
+
+
+export default function Footer() {
+    return (
+        <footer className={clsx(
+            "min-h-screen",
+            "flex flex-col items-center justify-between",
+            "p-24"
+        )}>
+            footer
+        </footer>
+    )
+}
