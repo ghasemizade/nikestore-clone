26f6c20cc1d3114694b722fc23bc37ef2f07cb18
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-12 12:10:45 +0330

    feat (header): add head component in component folder

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import clsx from "clsx";
+
+
+export default function Head() {
+    return (
+        <header className={clsx(
+            "min-h-screen",
+            "flex flex-col items-center justify-between",
+            "p-24"
+        )}>
+            header
+        </header>
+    )
+}
