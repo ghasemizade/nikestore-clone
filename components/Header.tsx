96bd6d10fbe0d2c505d667ce9418545b3e737d9f
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-25 23:33:49 +0330

    feat (cart icon)

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,11 @@ import Image from "next/image";
 import Logo from "../public/icons/nike-4-logo-svgrepo-com.svg"
 import Link from "next/link";
 import Menu from "./Menu";
+import { IoMdHeartEmpty } from 'react-icons/io'
+import { BsCart } from 'react-icons/bs'
+import { BiMenuAltRight } from 'react-icons/bi'
+import { VscChromeClose } from 'react-icons/vsc'
+
 
 export default function Head() {
     const [movileMenu, setMovileMenu] = useState(false)
@@ -40,6 +45,35 @@ export default function Head() {
                 </Link>
                 <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu}/>
             </Wrapper>
+
+            <div className={clsx(
+                "flex items-center",
+                "gap-2",
+                "text-black"
+            )}>
+                <div className={clsx(
+                    "w-8 md:w-12 h-8 md:h-12",
+                    "rounded-full",
+                    "flex justify-center items-center",
+                    "hover:bg-black/[0.05]",
+                    "cursor-pointer",
+                    "relative"
+                )}>
+                    <BsCart className={clsx(
+                        "text-[15px] md:text-[20px]"
+                    )}/>
+                    <div className={clsx(
+                        "h-[14px] md:h-[18px]",
+                        "min-w-[14px] md:min-w-[18px]",
+                        "bg-red-600",
+                        "rounded-full",
+                        "absolute top-1 left-5 md:left-7",
+                        "text-white text-[10px] md:text-[12px]",
+                        "flex justify-center items-center",
+                        "px-[2px]"
+                    )}>5</div>
+                </div>
+            </div>
         </header>
     )
 }
