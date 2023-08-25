9bf51dfbedcfffb30ca371b2a7f83678275839a3
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-25 22:14:29 +0330

    feat (header class)

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,24 @@
+'use client'
+import React, { useState } from "react"
 import clsx from "clsx";
 
 
 export default function Head() {
+    const [movileMenu, setMovileMenu] = useState(false)
+    const [showCatMenu, setShowCatMenu] = useState(false)
+    const [show, setShow] = useState("translate-y-0")
+    const [lastScrollY, setLastScrollY] = useState(0)
+    
+
     return (
         <header className={clsx(
-            "min-h-screen",
-            "flex flex-col items-center justify-between",
-            "p-24"
+            "w-full h-[50px]",
+            "md:h-[80px]",
+            "bg-white",
+            "flex justify-between items-center",
+            "z-20",
+            "sticky top-0",
+            "transition-transform duration-200"
         )}>
             header
         </header>
