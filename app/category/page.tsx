d8b82c1cbd8819a62e20c755854b3bef9bb707b8
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-02 00:24:11 +0330

    remove header and footer

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -6,7 +6,6 @@ import clsx from "clsx";
 export default function Category() {
   return (
     <>
-      <Head />
       <main className={clsx(
         "min-h-screen",
         "flex flex-col items-center justify-between",
@@ -14,7 +13,6 @@ export default function Category() {
       )}>
         Category
       </main>
-      <Footer />
     </>
   )
 }
