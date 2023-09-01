61eee1b9c4c496f9f7d30130fffe813156aeefa9
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-02 00:24:27 +0330

    add header and footer to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import Head from '@/components/Header'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +21,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Head />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
