08a021bce36205a4bded8d039a105bcd076fe725
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-02 00:43:51 +0330

    update page category

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -1,17 +1,29 @@
-import Footer from "@/components/Footer";
-import Head from "@/components/Header";
 import clsx from "clsx";
+import data from '@/components/Listdata'
+import Card from "@/components/card";
+import Wrapper from "@/components/Wrapper";
 
 
 export default function Category() {
+  
   return (
     <>
       <main className={clsx(
         "min-h-screen",
         "flex flex-col items-center justify-between",
         "p-24",
-      )}>
-        Category
+        )}>
+        <Wrapper className={clsx(
+          "grid md:grid-cols-3 grid-cols-1 gap-20",
+          )}>
+            {data.map(item => (
+              <Card 
+                key={item.id}
+                src={item.src}
+                title={item.title}
+                price={item.price}/>
+            ))}
+        </Wrapper>
       </main>
     </>
   )
